4231aad244e98bc3e5d15d274d18b801b52d0c02
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2020-02-05 09:59:16 +0900

    Update H_from_points.py

--- image_mosaic/H_from_points.py
+++ image_mosaic/H_from_points.py
@@ -28,10 +28,11 @@ EXAMPLE
 
 SEE ALSO
   https://github.com/misasa/image_mosaic
+  https://github.com/misasa/image_mosaic/blob/master/image_mosaic/H_from_points.py
 
 IMPLEMENTATION
   Orochi, version 9
-  Copyright (C) 2014 Okayama University
+  Copyright (C) 2014-2020 Okayama University
   License GPLv3+: GNU GPL version 3 or later
 
 HISTORY
