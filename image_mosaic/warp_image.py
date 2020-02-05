c1c425935269653970287def8d0afebe99d28e5e
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2020-02-05 10:00:55 +0900

    Update warp_image.py

--- image_mosaic/warp_image.py
+++ image_mosaic/warp_image.py
@@ -32,10 +32,11 @@ EXAMPLE
 
 SEE ALSO
   https://github.com/misasa/image_mosaic
+  https://github.com/misasa/image_mosaic/blob/master/image_mosaic/warp_image.py
 
 IMPLEMENTATION
   Orochi, version 9
-  Copyright (C) 2014 Okayama University
+  Copyright (C) 2014-2020 Okayama University
   License GPLv3+: GNU GPL version 3 or later
 
 HISTORY
